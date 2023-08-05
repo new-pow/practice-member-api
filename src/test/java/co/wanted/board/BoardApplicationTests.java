1c690a37eca7c0db6c9f27c86e0d54f033bfd3c7
saehim <saeilee.vxv@gmail.com>
2023-08-05 14:44:44 +0900

    Chore: init the Project
    - Refs. #1

---
+++ src/test/java/co/wanted/board/BoardApplicationTests.java
@@ -0,0 +1,14 @@
+package co.wanted.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
