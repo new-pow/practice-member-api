9928d2cdc31083b68e6554b2a962a0e4f6bc2633
saehim <saeilee.vxv@gmail.com>
2023-08-05 14:46:44 +0900

    Chore: test properties 설정
    - Refs. #1

--- src/test/java/co/wanted/board/BoardApplicationTests.java
+++ src/test/java/co/wanted/board/BoardApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BoardApplicationTests {
 
     @Test
