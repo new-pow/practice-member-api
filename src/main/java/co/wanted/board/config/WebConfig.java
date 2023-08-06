063275ec2b84655ec4e0b5bcb59d4454ef801605
saehim <saeilee.vxv@gmail.com>
2023-08-06 11:04:26 +0900

    Chore: webconfig 초안
    - Refs. #1

---
+++ src/main/java/co/wanted/board/config/WebConfig.java
@@ -0,0 +1,18 @@
+package co.wanted.board.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*") //TODO origin 해결
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH");
+    }
+}
