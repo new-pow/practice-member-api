ca5a8d6273d04d2c5357ae82e57f7f9f7d4c4a91
saehim <saeilee.vxv@gmail.com>
2023-08-09 21:31:57 +0900

    Feat: 회원가입시 비밀번호 암호화 로직 추가
    - #4

--- src/main/java/co/wanted/board/api/member/application/SignupService.java
+++ src/main/java/co/wanted/board/api/member/application/SignupService.java
@@ -1,6 +1,8 @@
 package co.wanted.board.api.member.application;
 
+import co.wanted.board.api.auth.applicaion.PasswordEncoder;
 import co.wanted.board.api.member.domain.Member;
+import co.wanted.board.api.member.domain.Password;
 import co.wanted.board.api.member.infrastructure.persistence.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,10 +12,14 @@ import org.springframework.stereotype.Service;
 public class SignupService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
 
     public Member signup(String email, String password, String username) {
-        Member preparedMember = Member.create(email, password, username);
+        Password encodedPassword = passwordEncoder.encrypt(password);
+        Member preparedMember = Member.create(email, encodedPassword, username);
+
+
         return memberRepository.save(preparedMember);
     }
 }
