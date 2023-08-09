602ba67c2d6ad83d34feb313ae968f1f0e28ee99
saehim <saeilee.vxv@gmail.com>
2023-08-09 21:32:41 +0900

    Refactor: import 간소화
    - Refs. #4

--- src/main/java/co/wanted/board/global/model/CreatedEntity.java
+++ src/main/java/co/wanted/board/global/model/CreatedEntity.java
@@ -6,8 +6,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.Instant;
 
 @Getter
