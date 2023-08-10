16a1f192b4593ffbd72e7fb5650c1441e8c43de7
saehim <saeilee.vxv@gmail.com>
2023-08-10 21:21:43 +0900

    Test: 중복된 유저 이름 예외 테스트
    - #10

--- src/test/java/co/wanted/board/api/member/application/MemberFacadeTest.java
+++ src/test/java/co/wanted/board/api/member/application/MemberFacadeTest.java
@@ -1,12 +1,11 @@
 package co.wanted.board.api.member.application;
 
+import co.wanted.board.api.member.application.exception.MemberException;
 import co.wanted.board.api.member.domain.Member;
-import co.wanted.board.api.member.exception.MemberException;
 import co.wanted.board.api.member.infrastructure.persistence.MemberRepository;
+import co.wanted.board.api.member.presentation.MemberFacade;
 import co.wanted.board.api.member.presentation.dto.Signup;
 import co.wanted.board.fixture.MemberFixtureFactory;
-import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -40,7 +39,18 @@ class MemberFacadeTest {
         memberRepository.save(testMember);
         String mail = "test@gmail.com";
 
-        assertThatThrownBy(() -> memberFacade.signup(new Signup.Request(mail, "newPassword1234", "새힘")))
+        assertThatThrownBy(() -> memberFacade.signNewMember(new Signup.Request(mail, "newPassword1234", "새힘")))
+                .isInstanceOf(MemberException.class);
+    }
+
+
+    @Test
+    @DisplayName("중복된 유저이름으로 회원가입을 시도하면 예외를 던진다.")
+    void givenDuplicatedUsername_thenThrowsException() {
+        memberRepository.save(testMember);
+        String name = "회원";
+
+        assertThatThrownBy(() -> memberFacade.signNewMember(new Signup.Request("testemail2@gmail.com", "newPassword1234", name)))
                 .isInstanceOf(MemberException.class);
     }
 }
