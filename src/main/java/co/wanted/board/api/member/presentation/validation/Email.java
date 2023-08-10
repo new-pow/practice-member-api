93deb953cae17fa91992e3913f5a69ab32593253
saehim <saeilee.vxv@gmail.com>
2023-08-10 21:09:34 +0900

    Fix: email 어노테이션 예외 수정
    - 기본적으로 있어야할 필드 생성
    
    - #10

--- src/main/java/co/wanted/board/api/member/presentation/validation/Email.java
+++ src/main/java/co/wanted/board/api/member/presentation/validation/Email.java
@@ -1,6 +1,7 @@
 package co.wanted.board.api.member.presentation.validation;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 import java.lang.annotation.*;
 
 @Target(ElementType.FIELD)
@@ -9,4 +10,6 @@ import java.lang.annotation.*;
 @Documented
 public @interface Email {
     String message() default "이메일 형식이 아닙니다. '@'를 포함하여야 합니다.";
+    Class<?>[] groups() default {}; // 기본적으로 Default 그룹에 속하도록 설정
+    Class<? extends Payload>[] payload() default {};
 }
