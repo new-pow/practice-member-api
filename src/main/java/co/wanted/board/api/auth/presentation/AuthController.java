148418f1d034c61763dc6b5bed4c87035375403e
saehim <saeilee.vxv@gmail.com>
2023-08-11 21:21:43 +0900

    Feat: presentation controller 구현
    - #8

---
+++ src/main/java/co/wanted/board/api/auth/presentation/AuthController.java
@@ -0,0 +1,30 @@
+package co.wanted.board.api.auth.presentation;
+
+import co.wanted.board.api.auth.application.jwt.JwtService;
+import co.wanted.board.api.auth.domain.Tokens;
+import co.wanted.board.api.auth.presentation.dto.Refresh;
+import co.wanted.board.global.model.BasicResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthFacade authFacade;
+
+    @PostMapping("/refresh")
+    public BasicResponse<Tokens> refreshMemberToken(@RequestBody Refresh.Request request, HttpServletResponse response) throws IOException {
+        authFacade.refreshToken(request, response);
+        return BasicResponse.send("AccessToken이 새로 갱신되었습니다.", null);
+    }
+}
