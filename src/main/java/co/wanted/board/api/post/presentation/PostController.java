0e10bc7dc738416ff9ac222f159a71e0bfb84f23
saehim <saeilee.vxv@gmail.com>
2023-08-15 22:34:13 +0900

    Feat: post 요청 presentation
    - #15

---
+++ src/main/java/co/wanted/board/api/post/presentation/PostController.java
@@ -0,0 +1,24 @@
+package co.wanted.board.api.post.presentation;
+
+import co.wanted.board.api.auth.presentation.dto.Logined;
+import co.wanted.board.api.post.presentation.dto.PostSelect;
+import co.wanted.board.api.post.presentation.dto.PostUpdate;
+import co.wanted.board.api.post.presentation.dto.PostWrite;
+import co.wanted.board.global.model.BasicResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/post")
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostFacade postFacade;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BasicResponse<PostWrite.Response> writePost(@RequestAttribute Logined logined, @RequestBody PostWrite.Request request) {
+        PostWrite.Response response = postFacade.writePost(logined, request);
+        return BasicResponse.send("글이 작성되었습니다.", response);
+    }
