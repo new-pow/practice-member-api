971dc2f6189571792469d1f4cdbc1e01843ef94d
saehim <saeilee.vxv@gmail.com>
2023-08-16 12:24:57 +0900

    Feat: 여러개의 PK로 member 조회 service
    - `@Transactional` 추가
    
    - #17

--- src/main/java/co/wanted/board/api/member/application/SearchService.java
+++ src/main/java/co/wanted/board/api/member/application/SearchService.java
@@ -7,6 +7,9 @@ import co.wanted.board.api.member.infrastructure.persistence.MemberRepository;
 import co.wanted.board.api.member.presentation.dto.Signup;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -22,10 +25,12 @@ public class SearchService {
         return memberRepository.existsByUsername(username);
     }
 
+    @Transactional(readOnly = true)
     public Member getMemberByEmail (String email) {
         return memberRepository.findByEmail(email).orElseThrow(() -> new MemberException(ErrorCode.NO_SUCH_MEMBER));
     }
 
+    @Transactional(readOnly = true)
     public void checkValidNewMember(Signup.Request request) {
         if (isExistMail(request.getEmail())) {
             throw new MemberException(ErrorCode.DUPLICATE_KEY_EMAIL);
@@ -39,7 +44,13 @@ public class SearchService {
         return memberRepository.existsById(memberId);
     }
 
+    @Transactional(readOnly = true)
     public Member getMemberById(Long authorId) {
         return memberRepository.findById(authorId).orElseThrow(() -> new MemberException(ErrorCode.NO_SUCH_MEMBER));
+    }
+
+    @Transactional(readOnly = true)
+    public Iterable<Member> getAllMembersByIds(Set<Long> authorIds) {
+        return memberRepository.findAllById(authorIds);
     }
 }
